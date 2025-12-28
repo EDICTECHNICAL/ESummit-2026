64fda984aa8aeaace57252974209996e8fed6b9b
Bhavika Vasule <bhavikavasule@gmail.com>
2025-12-28 14:46:40 +0530

    update angel investor page

--- src/components/pages/TAIR/componens/MemberCard.tsx
+++ src/components/pages/TAIR/componens/MemberCard.tsx
@@ -58,21 +58,21 @@ const MemberCard: React.FC<MemberCardProps> = ({ member }) => {
 
             {member.duty && (
                 <div className="member-contact duty">
-                    <Target size={17} className="contact-icon" />
+                    <Target size={18} className="contact-icon" color="#ff2d2d"/>
                     {member.duty}
                 </div>
             )}
 
             {showPhone && (
                 <div className="member-contact phone">
-                    <Phone size={17} className="contact-icon" />
+                    <Phone size={18} className="contact-icon" color="#ff2d2d" />
                     <a href={`tel:${member.phone}`}>{member.phone}</a>
                 </div>
             )}
 
             {showEmail && (
-                <div className="member-contact email">
-                    <Mail size={17} className="contact-icon" />
+                <div className="member-contact email" >
+                    <Mail size={18} className="contact-icon" color="#ff2d2d" />
                     <a href={`mailto:${member.email}`}>{member.email}</a>
                 </div>
             )}
